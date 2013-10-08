94cd9f20a8e58e2af546c074e02499a003582e98
bnell <bnell@24242424.local>
2013-10-08 09:40:28 -0400

    Sort results

--- pattdown.py
+++ pattdown.py
@@ -5,6 +5,7 @@
 from struct import unpack
 from os import path
 from optparse import OptionParser
+import operator
 
 def findpatt(handle,fsize,psize,offset=0):
 	# Read in a file handle, file size, pattern size, and offset and go find patterns.
@@ -38,35 +39,43 @@ def flattenpatt(pattern_tuple):
 		patternstring += "%0.2x" % byte
 	return patternstring
 
-parser = OptionParser()
-# File to parse
-parser.add_option("-f", "--file", action="store", dest="file_to_parse")
+def main():
+	parser = OptionParser()
+	# File to parse
+	parser.add_option("-f", "--file", action="store", dest="file_to_parse")
 
-# Number of bytes in a pattern
-parser.add_option("-n", "--number", action="store", dest="pattern_size", default=4)
+	# Number of bytes in a pattern
+	parser.add_option("-n", "--number", action="store", dest="pattern_size", default=4)
 
-# Filter out the number of repeats
-parser.add_option("-r", "--repeats", action="store", dest="repeats", default=4)
+	# Filter out the number of repeats
+	parser.add_option("-r", "--repeats", action="store", dest="repeats", default=20)
 
-# Specify an offset to start from
-parser.add_option("-o", "--offset", action="store", dest="offset", default=0)
+	# Specify an offset to start from
+	parser.add_option("-o", "--offset", action="store", dest="offset", default=0)
 
-(options, args) = parser.parse_args()
+	(options, args) = parser.parse_args()
 
-if not options.file_to_parse:
-	parser.print_help()
-	parser.error("File name required.")
+	if not options.file_to_parse:
+		parser.print_help()
+		parser.error("File name required.")
 
-else:
-	f = open(options.file_to_parse, 'rb')
-	print "Parsing..."
+	else:
+		f = open(options.file_to_parse, 'rb')
+		print "Parsing..."
 
-	# prototype, not worried about error handling
-	patterns = findpatt(f,path.getsize(options.file_to_parse),int(options.pattern_size),options.offset)
+		# prototype, not worried about error handling
+		patterns = findpatt(f,path.getsize(options.file_to_parse),int(options.pattern_size),options.offset)
 
-	for key in patterns.keys():
-		
-		if not patterns[key] < int(options.repeats):
-			print "Pattern: %s\tCount: %d" % (key,patterns[key])
-	print "Done parsing."
-	f.close()
+		print "Sorting..."
+		sorted_patterns = sorted(patterns.iteritems(), key=operator.itemgetter(1))
+
+		#for key in patterns.keys():
+		for patt in sorted_patterns:
+			if not patt[1] < int(options.repeats):
+				print "Pattern: %s\tCount: %d" % (patt[0],patt[1])
+		print "Done parsing."
+		f.close()
+
+
+if __name__ == "__main__":
+	main()
